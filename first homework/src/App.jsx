457aa74c2613c847cfbf1ce21b9a0f76988570e4
unknown <abu040810@gmail.com>
2023-08-03 23:09:17 +0500

    first commit

---
+++ first homework/src/App.jsx
@@ -0,0 +1,366 @@
+
+import "./App.css"
+
+function App() {
+
+
+  return (
+    <>
+        <header>
+            <div className="container">
+                <nav className="nav">
+                    <a href="./index.html" className="nav_logoo">
+                        NOICELAND
+                    </a>
+
+                    <ul className="nav_list">
+                        <li><a href="#" className="nav_link">Home</a></li>
+                        <li><a href="#" className="nav_link">Latest</a></li>
+                        <li><a href="#" className="nav_link">About</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+
+        <section id="intro">
+            <div className="container">
+                <div className="intro_wrapperr">
+                    <img src="https://picsum.photos/id/12/720/474/" alt=" image" />
+
+                    <div className="intro_texts">
+                        <span className="illustration">Illustration</span>
+
+                        <h1 className="intro_title">
+                            Japan House opens in mountainside to foster peak creativity.
+                        </h1>
+
+                        <p className="intro_text">
+                            Enim omittam qui id, ex quo atqui dictas complectitur. Nec ad timeam accusata, hinc justo falli id eum, ferri novum molestie eos cu.
+                        </p>
+
+                        <p className="author_name">By Reta Torphy</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="frames">
+            <div className="container">
+                <div className="f_wrapper">
+                    <div className="card">
+                        <img src="https://picsum.photos/id/43/375/254/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="card_xs">Photography</span>
+
+                            <h3 className="card_2xl">Helmut Lang celebrates taxi drivers worldwide in latest campaign</h3>
+
+                            <p className="card_author">By  Rosanna Ondricka</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/44/375/254/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="card_xs">Photography</span>
+
+                            <h3 className="card_2xl">Bowlcut launch a new summer collection that pays homage to “UK legends”</h3>
+
+                            <p className="card_author">By  Annie Lueilwitz</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/49/375/254/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="card_xs">Photography</span>
+
+                            <h3 className="card_2xl">Thousands of previously unseen photographs by Andy Warhol will be made public this Autumn</h3>
+
+                            <p className="card_author">By  Simeon Brekke</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/46/375/254/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="card_xs">Photography</span>
+
+                            <h3 className="card_2xl">London-based Yinka Ilori’s storytelling furniture</h3>
+
+                            <p className="card_author">By  Reta Torphy</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/47/375/254/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="card_xs">Photography</span>
+
+                            <h3 className="card_2xl">Anonymous Israeli art collective Broken Fingaz direct music video for U2 and Beck</h3>
+
+                            <p className="card_author">By  Reta Torphy</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/48/375/254/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="card_xs">Photography</span>
+
+                            <h3 className="card_2xl">Suzanne Saroff’s meticulously arranged photographs alter perceptions</h3>
+
+                            <p className="card_author">By  Alessandra Ortiz</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="posts">
+            <div className="container">
+                <h5>LATEST POSTS</h5>
+
+                <div className="p_wrapper">
+                    <div className="card">
+                        <img src="https://picsum.photos/id/43/285/185/" alt="Card image" />
+
+                        <div className="card_body_post">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Japan House opens in mountainside to foster peak creativity.</h3>
+
+                            <p className="p_author">By  Reta Torphy</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/44/285/185/" alt="Card image" />
+
+                        <div className="card_body_post">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Helmut Lang celebrates taxi drivers worldwide in latest campaign</h3>
+
+                            <p className="p_author">By  Alessandra Ortiz</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/49/285/185/" alt="Card image" />
+
+                        <div className="card_body_post">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Bowlcut launch a new summer collection that pays homage to “UK legends”</h3>
+
+                            <p className="p_author">By  Rosanna Ondricka</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/46/285/185/" alt="Card image" />
+
+                        <div className="card_body_post">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Thousands of previously unseen photographs by Andy Warhol will be made public this Autumn</h3>
+
+                            <p className="p_author">By  Annie Lueilwitz</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/47/285/185/" alt="Card image" />
+
+                        <div className="card_body_post">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">London-based Yinka Ilori’s storytelling furniture</h3>
+
+                            <p className="p_author">By  Annie Lueilwitz</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/48/285/185/" alt="Card image" />
+
+                        <div className="card_body_post">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Anonymous Israeli art collective Broken Fingaz direct music video for U2 and Beck</h3>
+
+                            <p className="p_author">By  Simeon Brekke</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/50/285/185/" alt="Card image" />
+
+                        <div className="card_body_post">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Suzanne Saroff’s meticulously arranged photographs alter perceptions</h3>
+
+                            <p className="card_author">By  Leo Bartell</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/51/285/185/" alt="Card image" />
+
+                        <div className="card_body_post">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Anu Ambasna’s playful illustrations celebrate club culture, brown bodies and perfect paunches</h3>
+
+                            <p className="p_author">By  Reta Torphy</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="btn_w">
+                    <button className="view_all">
+                        view all latest posts
+                    </button>
+                </div>
+            </div>
+        </section>
+
+
+        <section id="f_posts">
+            <div className="container">
+                <h5>featured POSTS</h5>
+
+                <div className="f1_wrapper">
+                    <div className="card">
+                        <img src="https://picsum.photos/id/52/570/387/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Helmut Lang celebrates taxi drivers worldwide in latest campaign</h3>
+
+                            <p className="p_author">By  Rosanna Ondricka</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/53/570/387/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Bowlcut launch a new summer collection that pays homage to “UK legends”</h3>
+
+                            <p className="p_author">By  Annie Lueilwitz</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/54/570/387/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">Thousands of previously unseen photographs by Andy Warhol will be made public this Autumn</h3>
+
+                            <p className="p_author">By  Simeon Brekke</p>
+                        </div>
+                    </div>
+
+                    <div className="card">
+                        <img src="https://picsum.photos/id/55/570/387/" alt="Card image" />
+
+                        <div className="card_body">
+                            <span className="p_title">Photography</span>
+
+                            <h3 className="p_tex">London-based Yinka Ilori’s storytelling furniture</h3>
+
+                            <p className="p_author">By  Reta Torphy</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+
+        <footer>
+            <div className="container">
+                <div className="f_top">
+                    <a href="#" className="footer_logo">
+                        NOICELAND
+                    </a>
+
+                    <p className="copywright">© 2018 Noiceland™, all rights reserved</p>
+                </div>
+
+                <div className="f_double">
+                    <p className="category c1">Categories</p>
+
+                    <p className="category">Subscribe to newsletter</p>
+                </div>
+
+                <div className="f_body">
+
+                    <div className="f_w1">
+
+                        <ul className="f_list">
+                            <li><a href="#" className="f_link">Animation</a></li>
+                            <li><a href="#" className="f_link">Interactive Design</a></li>
+                        </ul>
+
+                        <ul className="f_list">
+                            <li><a href="#" className="f_link">Architecture</a></li>
+                            <li><a href="#" className="f_link">Miscellaneous</a></li>
+                        </ul>
+
+                        <ul className="f_list">
+                            <li><a href="#" className="f_link">Graphic Design</a></li>
+                            <li><a href="#" className="f_link">Photography</a></li>
+                        </ul>
+
+                        <ul className="f_list">
+                            <li><a href="#" className="f_link">Illustration</a></li>
+                            <li><a href="#" className="f_link">Product Design</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <input type="email" placeholder="Email address" />
+                    </div>
+                </div>
+
+                <div className="f_bodyy">
+                    <ul className="f_list2">
+                        <p className="category">Information</p>
+                        <li><a href="#" className="f_link">about</a></li>
+                        <li><a href="#" className="f_link">contact</a></li>
+                        <li><a href="#" className="f_link">terms</a></li>
+                    </ul>
+
+                    <ul className="f_list2">
+                        <p className="category">follow us</p>
+                        <li><a href="#" className="f_link">instagram</a></li>
+                        <li><a href="#" className="f_link">facebook</a></li>
+                        <li><a href="#" className="f_link">twitter</a></li>
+                    </ul>
+
+                    <ul className="f_list2">
+                        <p className="category">Template</p>
+                        <li><a href="#" className="f_link">Image License Info</a></li>
+                        <li><a href="#" className="f_link">Powered by Webflow</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+
+    </>
+  )
+}
+
+export default App
